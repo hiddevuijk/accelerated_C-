feef838418729b2dca19961e23530465a5d23593
Hidde <hdvuijk@gmail.com>
2015-10-24 14:43:16 +0200

    added extract_fail function to Student_info.h and .cpp

--- ch5/Student_info.h
+++ ch5/Student_info.h
@@ -15,7 +15,7 @@ struct Student_info {
 bool compare(const Student_info&, const Student_info&);
 std::istream& read(std::istream&, Student_info&);
 std::istream& read_hw(std::istream&, std::vector<double>&);
-
+std::vector<Student_info> extract_fails(std::vector<Student_info>&);
 
 
 
