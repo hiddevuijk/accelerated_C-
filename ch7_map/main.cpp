8c028f90da0e79627039ef8b164a37bc478fb9cc
hiddevuijk <hdvuijk@gmail.com>
2015-12-04 17:05:03 +0100

    finished first part of ch7, about maps

---
+++ ch7_map/main.cpp
@@ -0,0 +1,53 @@
+#include <vector>
+#include <iostream>
+#include <map>
+#include <string>
+#include "split.h"
+#include <fstream>
+
+using namespace::std;
+
+
+map<string,vector<int> > xref(istream& in,
+	vector<string> find_words(const string&) = split)
+{
+	string line;
+	int line_number = 0;
+	map<string,vector<int> > ret;
+
+	while(getline(in, line)) {
+		++ line_number;
+		vector<string> words = find_words(line);
+
+		for(vector<string>::const_iterator it = words.begin();
+			it != words.end(); ++it) {
+			ret[*it].push_back(line_number);
+		}
+	}
+	return ret;
+}
+
+
+int main()
+{
+
+	map<string,vector<int> > ret = xref(cin);
+	for(map<string,vector<int> >::const_iterator it = ret.begin();
+		it != ret.end();++it) {
+
+		cout << it->first << " occurs on line(s): ";
+
+		vector<int>::const_iterator line_it=it->second.begin();
+		cout << *line_it;
+		++line_it;
+		while(line_it!=it->second.end()) {
+			cout << ", " << *line_it;
+			++line_it;
+		}
+		cout << endl;
+	}
+		
+
+	return 0;
+}
+
