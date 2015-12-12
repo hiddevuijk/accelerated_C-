51b34f991c2abf67b89807ae8d49caf9e7806d60
hiddevuijk <hdvuijk@gmail.com>
2015-12-12 13:10:40 +0100

    finished ch10 up to 10.3

---
+++ ch11/Vec.h
@@ -0,0 +1,34 @@
+#ifndef GUARD_Vec_h
+#define GUARD_Vec_h
+
+template <class T> class Vec {
+public:
+	typedef T* iterator;
+	typedef const T* const_iterator;
+	typedef size_t size_type;
+	typedef T value_type;
+	typedef std::ptrdiff_t difference_type;
+	typedef T& reference;
+	typedef const T& const_reference;
+
+	Vec() { create(); }
+	explicit Vec(std::size_t n, const T& val = T()) { create(n,val); }
+
+	size_type size() const { return limit - data; }
+
+	T& operator[](size_type i) {return data[i]; }
+	const T& operator[](size_type i) const { return data[i]; }
+
+	iterator begin() { return data; }
+	const_iterator begin() const { return data; }
+
+	iterator end() { return limit; }
+	const_iterator end() const { return limit; }
+
+private:
+	iterator data;
+	iterator limit;
+};
+
+
+#endif
---
+++ ch11/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+
+#include "Student_info.h"
+#include "Vec.h"
+
+using namespace::std;
+
+int main()
+{
+
+
+	return 0;
+}
