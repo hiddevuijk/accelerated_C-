c353290b9772a9966cf0f10d340f258f6419c616
hiddevuijk <hiddevuijk@hiddevuijk-ThinkPad-T400>
2018-01-05 22:44:40 +0100

    finished ch11

---
+++ ch11_2/main.cpp
@@ -0,0 +1,20 @@
+
+#include "vec.h"
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+
+using namespace std;
+
+
+int main()
+{	
+	Vec<int> v1(10,1);
+	Vec<int> v2(10,2);
+
+
+	return 0;
+}
